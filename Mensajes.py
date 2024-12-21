4016f2f9358633bd27903e4bbd2a6dc2b8aab75a
Luis Garrido <luis.garrido@fashionspark.com>
2024-12-21 20:38:18 -0300

    railway

--- Mensajes.py
+++
@@ -1,2 +0,0 @@
-class Mensajes:
-    SALUDO = "Hola Mundo"
--- Users.py
+++
@@ -1,7 +0,0 @@
-users= [
-    {"name": "Vicente"},
-    {"name": "Gaspar"},
-    {"name": "Matilde"},
-    {"name": "Luis"},
-    {"name": "Ximena"}
-]
